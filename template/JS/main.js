8314383d415ba87f087b284d7fd789d290a7e86f
Mercedes <mercedespontaut@gmail.com>
2022-12-14 23:02:25 -0300

    header y footer

---
+++ template/JS/main.js
@@ -0,0 +1,48 @@
+sup=`
+<header>
+<div class="h-box">
+    <div class="col-sm-12 col-m-12 col-12">
+        <a href="./index.html"><h1 class="h-h1">BookMatch</h1></a>
+    </div>
+    <div class="col-sm-12 col-m-12 col-12">
+        <a href="./index.html"><div class="h-a col-3 col-m-3 col-sm-12">Home</div></a>
+        <a href="./who.html"><div class="h-a col-3 col-m-3 col-sm-12">Quienes somos</div></a>
+        <a href="./books.html"><div class="h-a col-3 col-m-3 col-sm-12">Libros</div></a>
+        <a href="./thanks.html"><div class="h-a col-3 col-m-3 col-sm-12">Agradecimientos</div></a>
+    </div>
+</div>
+</header>
+`
+document.write(sup)
+
+fut=`
+<footer>
+<div class="f-box col-12 col-m-12 col-sm-12">
+    <div class="col-4 col-m-4 col-sm-12">
+        <div>
+            <h4>BookMatch</h4>
+        </div>
+        <div class="f-p">
+            <p>Nuestra satisfacción se encuentra en el contentamiento de nuestros usuarios, agradecemos la participación de todos y cada uno de ellos. <br> Este fue un proyecto producido para el programa de educación "Codo a Codo".</p>
+        </div>
+    </div>
+    <div class="f-imgCaC col-4 col-m-4 col-sm-12">
+        <a href="https://www.buenosaires.gob.ar/educacion/codo-codo" target="_blank"><img src="./img/codoacodo.png" alt=""></a>
+    </div>
+    <div class="col-4 col-m-4 col-sm-12">
+        <div>
+            <h4>Contacto</h4>
+        </div>
+        <div class="f-pad f-p">
+            <p>Trabajo hecho por Mercedes Llanos Pontaut. <br> En los siguientes iconos podrá encontrar los medios de contacto:</p>
+        </div>
+    </div>
+</div>
+<div class="f-icons">
+    <a href="mailto:mercedespontaut@gmail.com" target="_blank" class="f-icon"><img src="./img/gm.png" width="35px"></a>
+    <a href="https://wa.me/542923517800" target="_blank" class="f-icon"><img src="./img/wpp.png" width="35px"></a>
+    <a href="https://github.com/Mercedesllp" target="_blank" class="f-icon"><img src="./img/gh.png" width="35px"></a>
+</div>
+</footer>
+`
+document.write(fut)
